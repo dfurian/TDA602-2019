7dddd3a3254e12d0a456d84af30c9ff01b657966
Denis <furian@student.chalmers.se>
2019-04-19 17:12:48 +0200

    Lab1/Denis

--- Lab1-TOCTOU/Wallet.java
+++ Lab1-TOCTOU/Wallet.java
@@ -1,5 +1,7 @@
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.RandomAccessFile;
 import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
@@ -50,36 +52,73 @@ public class Wallet {
 	 * @throws Exception if balance is insufficient
 	 */
 	public synchronized void safeWithdraw(int valueToWithdraw) throws Exception {
+		BufferedReader scanner = new BufferedReader(new InputStreamReader(System.in));
 		FileChannel fileChannel = file.getChannel();
-		System.out.println("asking for lock");
-		FileLock lock = fileChannel.lock();
+		System.out.println("asking for lock for file " + file.toString());
+		FileLock lock = fileChannel.tryLock();
+		while (true) {
+			if (lock == null) {
+				System.out.println("the file is not available");
+				System.out.println("press [A] to acquire lock again");
+				System.out.println("press [R] to release lock");
+				lock = fileChannel.tryLock();
+			} else if (lock.isValid()) {
+				System.out.println("the file is available for read/write operations");
+				System.out.println("press [R] to release lock");
+			} else {
+				System.out.println("the lock is not valid");
+				System.out.println("press [A] to acquire lock again");
+				lock.close();
+				lock = fileChannel.tryLock();
+			}
+			System.out.println("press [X] to exit");
+			String input = scanner.readLine().trim().toUpperCase();
+			if ("A".equals(input)) {
+				if( lock != null ) {
+					lock.close();
+				}
+				lock = fileChannel.tryLock();
+			} else if ("R".equals(input)) {
+				if( lock != null ) {
+					lock.close();
+				}
+			} else if ("X".equals(input)) {
+				return;
+			} else {
+				System.out.println("Unknown command " + input);
+			}
+		}
+
+//		FileChannel fileChannel = file.getChannel();
+//		System.out.println("asking for lock");
+//		FileLock lock = fileChannel.tryLock();
 //		while (true) {
-//			if (lock != null) {
+//			if (lock == null || !lock.isValid()) {
 //				System.out.println("Another instance is already running");
 //				lock = fileChannel.tryLock();
 //			} else {
 //				break;
 //			}
 //		}
-		// read balance from file
-		System.out.println("acquired lock");
-		int balance = getBalance();
-		Thread.sleep(10000);
-		// verify the amount
-		if (balance < valueToWithdraw) {
-			// exception if balance is low
-			throw new Exception("Insufficient balance");
-		} else {
-			int newBalance = balance - valueToWithdraw;
-			// update balance otherwise
-			try {
-				setBalance(newBalance);
-			} finally {
-				if (lock != null) {
-					lock.release();
-				}
-			}
-		}
+//		// read balance from file
+//		System.out.println("acquired lock");
+//		int balance = getBalance();
+//		Thread.sleep(10000);
+//		// verify the amount
+//		if (balance < valueToWithdraw) {
+//			// exception if balance is low
+//			throw new Exception("Insufficient balance");
+//		} else {
+//			int newBalance = balance - valueToWithdraw;
+//			// update balance otherwise
+//			try {
+//				setBalance(newBalance);
+//			} finally {
+//				if (lock != null) {
+//					lock.release();
+//				}
+//			}
+//		}
 	}
 
 	/**
