d29481842cf86860be868951490f7d2563171e82
Denis Furian <35733177+dfurian@users.noreply.github.com>
2019-04-19 23:01:17 +0200

    Update of API to Pocket.java

--- Lab1-TOCTOU/Pocket.java
+++ Lab1-TOCTOU/Pocket.java
@@ -1,7 +1,9 @@
 import java.io.File;
 import java.io.IOException;
 import java.io.RandomAccessFile;
-
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+import java.nio.channels.FileLock;
 
 public class Pocket {
     /**
@@ -20,12 +22,17 @@ public class Pocket {
 
     /**
      * Adds a product to the pocket. 
+     * This method was upgraded to be mutual exclusive, we didn't make a new safeAddProduct since it still won't be worth anything if the ShoppingCart uses the unsafe withdrawal method...
      *
      * @param  product           product name to add to the pocket (e.g. "car")
      */
     public void addProduct(String product) throws Exception {
+	FileLock apparentlyThisPocketHasSomeLockOrMightWeCallItZipperInsteadHahaSoFunnyPleaseLikeAndSubscribeToMyChannel = file.getChannel().lock();
+	// now that we are inside the pocket, we can add the product
         this.file.seek(this.file.length());
-        this.file.writeBytes(product+'\n'); 
+        this.file.writeBytes(product+'\n');
+	// we're done writing: we can release the lock
+	apparentlyThisPocketHasSomeLockOrMightWeCallItZipperInsteadHahaSoFunnyPleaseLikeAndSubscribeToMyChannel.channel().close();
     }
 
     /**
